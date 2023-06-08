8304ad6e9e08f7364031e4ab766dea9af9955d9b
irubioph <delightsmediaph@gmail.com>
2023-06-09 07:39:50 +0800

    Lesson-4-Navbar-and-Links

---
+++ src/pages/components/Navbar.js
@@ -0,0 +1,14 @@
+import { Link } from 'gatsby'
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <nav>
+        <h1>IvanRubio</h1>
+        <div className="links"></div>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/projects">Portfolio Projects</Link>
+    </nav>
+  )
+}
