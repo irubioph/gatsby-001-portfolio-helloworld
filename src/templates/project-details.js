7d9a094a2dba9d23026f3bfb4669d453e22a2f0f
irubioph <delightsmediaph@gmail.com>
2023-06-14 19:54:19 +0800

    Lesson-18-Template-Components

---
+++ src/templates/project-details.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import * as styles from '../styles/project-details.module.css'
+
+
+export default function ProjectDetails() {
+  return (
+    <Layout>
+        <div className={styles.details}>
+            <h2>title</h2>
+            <h3>stack</h3>
+            <div className={styles.featured}>
+                {/* <GatsbyImage image={featuredImg.childImageSharp.gatsbyImageData} alt="featured image" /> */}
+            </div>
+            {/* <div class={styles.html} dangerouslySetInnerHTML={{ __html: html }} /> */}
+        </div>
+    </Layout>
+  )
+}
